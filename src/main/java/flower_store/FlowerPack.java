1484f5972f9035a0d46b0e79b8c80e0c2e389cb2
Admin <mkruhliy@gmail.com>
2022-10-13 17:05:46 +0300

    v0.4

---
+++ src/main/java/flower_store/FlowerPack.java
@@ -0,0 +1,14 @@
+package flower_store;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class FlowerPack {
+    private Flower flower;
+    private int amount;
+
+    public double getPrice(){
+        return flower.getPrice()*amount;
+    }
+}
